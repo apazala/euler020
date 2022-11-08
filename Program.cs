e92a1d3e46730b84f8ece0c3e2e69d255ded91b8
apazala <albertopascual@hotmail.es>
2022-11-08 18:39:40 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BigInteger b = 1;
+        for(int i = 2; i <= 100; i++)
+            b*=i;
+
+        BigInteger sum = 0;
+        while(b>0){
+            BigInteger rem;
+            b = BigInteger.DivRem(b, 10, out rem);
+            sum += rem;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
